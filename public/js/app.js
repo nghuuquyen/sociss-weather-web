2bc32c6a381b285df2a1bb3415bcdb02e1817458
Quyen Nguyen Huu <nghuuquyen@gmail.com>
2017-11-15 18:23:27 +0700

    Implement basic client query

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,49 @@
-function showWeather(e) {
-  console.log(e.target);
+bindingClickEventToMap();
+
+/**
+* @name bindingClickEventToMap
+* @description
+* Binding click event to map area for handle get information.
+*/
+function bindingClickEventToMap() {
+  var items = document.getElementById("map-areas").children;
+
+  for(var i = 0 ; i < items.length; i ++) {
+    items[i].addEventListener('click', showWeather);
+  }
+}
+
+/**
+* @name showWeather
+* @description
+* Show weather modal of selected city.
+*
+* @param  {object} event Mouse clicked event.
+*/
+function showWeather(event) {
+  var _cityName = event.target.getAttribute('alt');
+
+  getCityWeather(_cityName, function(httpResponse) {
+    alert('Info: ' + httpResponse.responseText);
+  });
+}
+
+/**
+* @name getCityWeather
+* @description
+* Get city weather by city name.
+*
+* @param  {string}   cityName City name for get weather
+* @param  {Function} cb       callback after got data
+*/
+function getCityWeather(cityName, cb) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      cb(xhttp);
+    }
+  };
+
+  xhttp.open('GET', '/api/weather?city=' + cityName, true);
+  xhttp.send();
 }
--- server.js
+++ server.js
@@ -4,9 +4,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const fs = require('fs');
+const weather = require('./lib/WeatherTCPService');
+const logger = require('./lib/logger');
 
 // Set static content.
 app.use('/', express.static('./public'));
+
+app.use('/api/weather', function(req, res) {
+  const cityName = req.query.city || '';
+  logger.info(`Called API for get weather city ${cityName}`);
+
+  weather.getCityWeather(cityName).then(data => {
+    res.writeHead(200, { 'Content-type' : 'text/html' });
+    res.write(data);
+    res.end();
+  });
+});
 
 app.use(function renderHomepage(req, res, next) {
   fs.readFile(__dirname + '/public/index.html', function(data) {
@@ -17,9 +30,9 @@ app.use(function renderHomepage(req, res, next) {
 
 app.listen(port, function(err) {
   if(err) {
-    console.error('Something error !!');
-    console.error(err);
+    logger.error('Something error !!');
+    logger.error(err);
   }
 
-  console.log('App listen on port ' + port);
+  logger.info('App listen on port ' + port);
 });
